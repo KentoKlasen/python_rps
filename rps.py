fd373ba26b498d7b6f51304512f266feb7056872
Kento Klasen <kento.klasen@gmail.com>
2021-08-15 13:07:17 -0700

    testing out printing

---
+++ rps.py
@@ -0,0 +1,25 @@
+def main():
+	print('hello world')
+	print_heloo_world()	
+
+def print_heloo_world():
+	for i in range(5):
+		print('hell world')
+	print('goodbye world')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+	main()
